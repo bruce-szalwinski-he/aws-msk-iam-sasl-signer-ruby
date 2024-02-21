d4fb4bff038a89854d3823436e7c2df3789907b8
bruce szalwinski <bruce.szalwinski@hotelengine.com>
2024-02-21 09:44:32 -0600

    assert_equal, expected on the left

--- test/aws/msk/iam/sasl/signer_test.rb
+++ test/aws/msk/iam/sasl/signer_test.rb
@@ -25,26 +25,26 @@ class Aws::Msk::Iam::Sasl::SignerTest < Minitest::Test
       params = URI.decode_www_form(String(uri.query))
       params = Hash[ params.group_by(&:first).map{ |k,a| [k,a.map(&:last)] } ]
 
-      assert params["Action"] = "kafka-cluster:Connect"
-      assert params["X-Amz-Algorithm"] =  "AWS4-HMAC-SHA256"
-      assert params["X-Amz-Security-Token"] = "session_token"
-      assert params["X-Amz-SignedHeaders"] = "host"
-      assert params["X-Amz-Expires"] = "900"
+      assert_equal"kafka-cluster:Connect", params["Action"][0]
+      assert_equal "AWS4-HMAC-SHA256", params["X-Amz-Algorithm"][0]
+      assert_equal "session_token", params["X-Amz-Security-Token"][0]
+      assert_equal "host", params["X-Amz-SignedHeaders"][0]
+      assert_equal "900", params["X-Amz-Expires"][0]
       assert_match "aws-msk-iam-sasl-signer-ruby", params["User-Agent"][0]
 
       credentials = params["X-Amz-Credential"][0]
       split_credentials = credentials.split("/")
-      assert split_credentials[0] = @creds.access_key_id
-      assert split_credentials[2] = "us-east-1"
-      assert split_credentials[3] = "kafka-cluster"
-      assert split_credentials[4] = "aws4_request"
+      assert_equal  @creds.access_key_id, split_credentials[0]
+      assert_equal "us-east-1", split_credentials[2]
+      assert_equal "kafka-cluster", split_credentials[3]
+      assert_equal "aws4_request", split_credentials[4]
 
       date_obj = DateTime.strptime(params["X-Amz-Date"][0], "%Y%m%dT%H%M%SZ")
       current_time = Time.now.utc.to_i
       assert_lte date_obj.to_time.to_i, current_time
 
-      actual_expires = 1000 * (params["X-Amz-Expires"].to_i + date_obj.to_time.to_i)
-      assert actual_expires = expiration_time_ms
+      actual_expires = 1000 * (params["X-Amz-Expires"][0].to_i + date_obj.to_time.to_i)
+      assert_equal expiration_time_ms, actual_expires
 
     end
   end
