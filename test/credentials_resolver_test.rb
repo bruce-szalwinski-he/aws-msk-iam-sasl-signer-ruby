fd5bfff797d0ee1488cfeaf7d7c3ba7dc7ec6286
bruce szalwinski <bruce.szalwinski@hotelengine.com>
2024-02-26 20:57:56 -0600

    names are hard

--- test/credentials_resolver_test.rb
+++ test/credentials_resolver_test.rb
@@ -4,11 +4,11 @@ require "test_helper"
 require "aws-sdk-kafka"
 require "minitest/autorun"
 
-class AwsMskIamSaslSigner::CredentialResolverTest < Minitest::Test
+class AwsMskIamSaslSigner::CredentialsResolverTest < Minitest::Test
   def test_from_credential_provider_chain_raises_error_when_no_credentials
     stub = Aws::Kafka::Client.new(stub_responses: true, credentials: nil)
     Aws::Kafka::Client.stub :new, stub do
-      resolver = AwsMskIamSaslSigner::CredentialsProviderResolver.new
+      resolver = AwsMskIamSaslSigner::CredentialsResolver.new
       assert_raises(RuntimeError) { resolver.from_credential_provider_chain("us-east-1") }
     end
   end
@@ -16,7 +16,7 @@ class AwsMskIamSaslSigner::CredentialResolverTest < Minitest::Test
   def test_from_credential_provider_chain_success
     stub = Aws::Kafka::Client.new(stub_responses: true)
     Aws::Kafka::Client.stub :new, stub do
-      resolver = AwsMskIamSaslSigner::CredentialsProviderResolver.new
+      resolver = AwsMskIamSaslSigner::CredentialsResolver.new
       credentials = resolver.from_credential_provider_chain("us-east-1")
       assert_kind_of Aws::Credentials, credentials
     end
@@ -25,7 +25,7 @@ class AwsMskIamSaslSigner::CredentialResolverTest < Minitest::Test
   def test_from_profile
     creds = Aws::Credentials.new("access_key_id", "secret_access", "session_token")
     Aws::SharedCredentials.stub :new, creds do
-      resolver = AwsMskIamSaslSigner::CredentialsProviderResolver.new
+      resolver = AwsMskIamSaslSigner::CredentialsResolver.new
       credentials = resolver.from_profile("test-profile")
       assert_kind_of Aws::Credentials, credentials
     end
@@ -34,12 +34,12 @@ class AwsMskIamSaslSigner::CredentialResolverTest < Minitest::Test
   def test_from_role_arn
     stub = Aws::STS::Client.new(stub_responses: true)
     Aws::STS::Client.stub :new, stub do
-      resolver = AwsMskIamSaslSigner::CredentialsProviderResolver.new
-      credentials = resolver.from_role_arn(
+      resolver = AwsMskIamSaslSigner::CredentialsResolver.new
+      credentials_provider = resolver.from_role_arn(
         role_arn: "arn:aws-msk-iam-sasl-signer:iam::123456789012:role/role-name",
         session_name: "test-session"
       )
-      assert_kind_of Aws::STS::Types::Credentials, credentials
+      assert_respond_to credentials_provider, :credentials
     end
   end
 end
